210127d971fb2fe7292cb291f0b1b27b45d90762
Leo Chen <Leo@zhangbudademao.com>
2020-03-12 15:11:14 +0800

    代码整理

--- Raffle.cs
+++ Raffle.cs
@@ -1,24 +1,33 @@
-﻿using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace BiliRaffle
 {
-    class Raffle
+    internal class Raffle
     {
+        #region Private Fields
+
         private static string _Cookies;
-        public static string Cookies {
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        public static string Cookies
+        {
             get
             {
                 if (string.IsNullOrEmpty(_Cookies))
                 {
                 re:
-                    if ((bool)App.Current.Dispatcher.Invoke(() => {
+                    if ((bool)App.Current.Dispatcher.Invoke(() =>
+                    {
                         return (new LoginWindow()).ShowDialog();
                     }))
                         return _Cookies;
@@ -36,13 +45,17 @@ namespace BiliRaffle
             }
         }
 
+        #endregion Public Properties
+
+        #region Public Methods
+
         /// <summary>
         /// 开始抽奖
         /// </summary>
         /// <param name="url">Url</param>
         /// <param name="num">中奖人数</param>
         /// <param name="OneChance">只有一次机会</param>
-        public static void Start( string url,int num = 1, bool OneChance = false,bool CheckFollow = false)
+        public static void Start(string url, int num = 1, bool OneChance = false, bool CheckFollow = false)
         {
             ViewModel.Main.PushMsg("---------抽奖开始---------");
 
@@ -78,7 +91,7 @@ namespace BiliRaffle
                     break;
             }
         }
-        
+
         /// <summary>
         /// 开始抽奖（异步）
         /// </summary>
@@ -95,10 +108,10 @@ namespace BiliRaffle
             {
                 case "t.bilibili.com":
                     ViewModel.Main.PushMsg("---------抽奖设置---------");
-                    ViewModel.Main.PushMsg("抽奖地址：" + url + "\r\n抽奖类型：动态转发抽奖\r\n中奖人数：" + num + "\r\n不统计重复："+OneChance.ToString());
+                    ViewModel.Main.PushMsg("抽奖地址：" + url + "\r\n抽奖类型：动态转发抽奖\r\n中奖人数：" + num + "\r\n不统计重复：" + OneChance.ToString());
                     ViewModel.Main.PushMsg("---------抽奖信息---------");
 
-                    int[] rs = await T_RaffleAsync(tmp[3],num,OneChance,CheckFollow);
+                    int[] rs = await T_RaffleAsync(tmp[3], num, OneChance, CheckFollow);
 
                     ViewModel.Main.PushMsg("---------中奖名单---------");
                     foreach (int i in rs)
@@ -120,6 +133,10 @@ namespace BiliRaffle
                     break;
             }
         }
+
+        #endregion Public Methods
+
+        #region Private Methods
 
         /// <summary>
         /// 获取cookies实例
@@ -149,13 +166,100 @@ namespace BiliRaffle
         }
 
         /// <summary>
+        /// 通过Uid获取UName
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <returns></returns>
+        private static string GetUName(int uid)
+        {
+            string str = Http.GetBody("https://api.bilibili.com/x/space/acc/info?mid=" + uid);
+            if (!string.IsNullOrEmpty(str))
+            {
+                JObject obj = JObject.Parse(str);
+                if ((int)obj["code"] == 0)
+                {
+                    return obj["data"]["name"].ToString();
+                }
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// 判断是否为粉丝
+        /// </summary>
+        /// <param name="uid">uid</param>
+        /// <returns>是否</returns>
+        private static bool IsFollowing(int uid)
+        {
+            if (!string.IsNullOrEmpty(Cookies))
+            {
+                string str = Http.GetBody("https://api.bilibili.com/x/space/acc/relation?mid=" + uid, GetCookies(Cookies));
+                if (!string.IsNullOrEmpty(str))
+                {
+                    JObject obj = JObject.Parse(str);
+                    if ((int)obj["code"] == 0)
+                    {
+                        switch ((int)obj["data"]["be_relation"]["attribute"])
+                        {
+                            case 1://悄悄关注
+                            case 2://关注
+                            case 6://互关
+                                return true;
+
+                            default:
+                                ViewModel.Main.PushMsg("抽到【" + GetUName(uid) + "（uid:" + uid + "）】中奖，但未关注，结果无效。(relation:" + obj["data"]["be_relation"]["attribute"].ToString() + ")");
+                                return false;
+                        }
+                    }
+                }
+            }
+            ViewModel.Main.PushMsg("抽到【" + GetUName(uid) + "（uid:" + uid + "）】中奖，但未关注，结果无效。");
+            return false;
+        }
+
+        /// <summary>
+        /// 检查是否抽奖号
+        /// </summary>
+        /// <param name="uid">账号uid</param>
+        /// <returns>是否</returns>
+        private static bool IsRaffleId(int uid)
+        {
+            int raffle_count = 0;
+            Regex reg = new Regex("抽奖");
+            string str = Http.GetBody("https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?visitor_uid=0&host_uid=" + uid + "&offset_dynamic_id=0");
+            if (!string.IsNullOrEmpty(str))
+            {
+                JObject obj = JObject.Parse(str);
+                if ((int)obj["code"] == 0)
+                {
+                    Dynamic_Data data = JsonConvert.DeserializeObject<Dynamic_Data>(obj["data"].ToString());
+                    int check_count = data.cards.Length >= 10 ? 10 : data.cards.Length;
+                    for (int i = 0; i < check_count; i++)
+                    {
+                        if (reg.IsMatch(data.cards[i].card))
+                        {
+                            raffle_count++;
+                        }
+                    }
+                }
+            }
+
+            if (raffle_count > 5)
+            {
+                ViewModel.Main.PushMsg("抽到【" + GetUName(uid) + "（uid:" + uid + "）】中奖，但判定为抽奖号，结果无效。（指数：" + raffle_count + "/10）");
+                return true;
+            }
+            else return false;
+        }
+
+        /// <summary>
         /// 动态抽奖
         /// </summary>
         /// <param name="id">动态id(c_id)</param>
         /// <param name="num">中奖人数</param>
         /// <param name="OneChance">只有一次机会</param>
         /// <returns>抽奖结果</returns>
-        private static int[] T_Raffle(string id, int num,bool OneChance = false, bool CheckFollow = false)
+        private static int[] T_Raffle(string id, int num, bool OneChance = false, bool CheckFollow = false)
         {
             T_Repost_Data Data = new T_Repost_Data();
             List<int> uids = new List<int>();
@@ -219,6 +323,7 @@ namespace BiliRaffle
             }
             return rs;
         }
+
         /// <summary>
         /// 动态抽奖(异步)
         /// </summary>
@@ -246,7 +351,7 @@ namespace BiliRaffle
 
                             if (i == 0) ViewModel.Main.PushMsg("共有" + Data.total_count + "条转发。");
 
-                            if(Data.comments.Length != 0)
+                            if (Data.comments.Length != 0)
                             {
                                 foreach (T_Repost_Data.comment comment in Data.comments)
                                 {
@@ -294,105 +399,33 @@ namespace BiliRaffle
             });
         }
 
-        /// <summary>
-        /// 判断是否为粉丝
-        /// </summary>
-        /// <param name="uid">uid</param>
-        /// <returns>是否</returns>
-        private static bool IsFollowing(int uid)
-        {
-            if (!string.IsNullOrEmpty(Cookies))
-            {
-                string str = Http.GetBody("https://api.bilibili.com/x/space/acc/relation?mid=" + uid, GetCookies(Cookies));
-                if (!string.IsNullOrEmpty(str))
-                {
-                    JObject obj = JObject.Parse(str);
-                    if((int)obj["code"] == 0)
-                    {
-                        switch ((int)obj["data"]["be_relation"]["attribute"])
-                        {
-                            case 1://悄悄关注
-                            case 2://关注
-                            case 6://互关
-                                return true;
-                            default:
-                                ViewModel.Main.PushMsg("抽到【" + GetUName(uid) + "（uid:" + uid + "）】中奖，但未关注，结果无效。(relation:" + obj["data"]["be_relation"]["attribute"].ToString() + ")");
-                                return false;
-                        }
-                        
-                    }
-                }
-            }
-            ViewModel.Main.PushMsg("抽到【" + GetUName(uid) + "（uid:" + uid + "）】中奖，但未关注，结果无效。");
-            return false;
-        }
+        #endregion Private Methods
 
-        /// <summary>
-        /// 检查是否抽奖号
-        /// </summary>
-        /// <param name="uid">账号uid</param>
-        /// <returns>是否</returns>
-        private static bool IsRaffleId(int uid)
-        {
-            int raffle_count = 0;
-            Regex reg = new Regex("抽奖");
-            string str = Http.GetBody("https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?visitor_uid=0&host_uid=" + uid + "&offset_dynamic_id=0");
-            if (!string.IsNullOrEmpty(str))
-            {
-                JObject obj = JObject.Parse(str);
-                if((int)obj["code"] == 0)
-                {
-                    Dynamic_Data data = JsonConvert.DeserializeObject<Dynamic_Data>(obj["data"].ToString());
-                    int check_count = data.cards.Length >= 10 ? 10 : data.cards.Length;
-                    for (int i = 0; i < check_count; i++)
-                    {
-                        if (reg.IsMatch(data.cards[i].card))
-                        {
-                            raffle_count++;
-                        }
-                    }
-                }
-            }
-
-            if (raffle_count > 5) {
-                ViewModel.Main.PushMsg("抽到【" + GetUName(uid) + "（uid:" + uid + "）】中奖，但判定为抽奖号，结果无效。（指数：" + raffle_count + "/10）");
-                return true;
-            }
-            else return false;
-
-        }
-
-        /// <summary>
-        /// 通过Uid获取UName
-        /// </summary>
-        /// <param name="uid"></param>
-        /// <returns></returns>
-        private static string GetUName(int uid)
-        {
-            string str = Http.GetBody("https://api.bilibili.com/x/space/acc/info?mid=" + uid);
-            if (!string.IsNullOrEmpty(str))
-            {
-                JObject obj = JObject.Parse(str);
-                if((int)obj["code"] == 0)
-                {
-                    return obj["data"]["name"].ToString();
-                }
-            }
-            return "";
-        }
+        #region Private Classes
 
         /// <summary>
         /// 动态数据模板
         /// </summary>
         private class Dynamic_Data
         {
-                public Card[] cards;
+            #region Public Fields
 
-                public class Card
-                {
-                    public string card;
-                }
+            public Card[] cards;
 
+            #endregion Public Fields
+
+            #region Public Classes
+
+            public class Card
+            {
+                #region Public Fields
+
+                public string card;
+
+                #endregion Public Fields
+            }
+
+            #endregion Public Classes
         }
 
         /// <summary>
@@ -400,14 +433,28 @@ namespace BiliRaffle
         /// </summary>
         private class T_Repost_Data
         {
-            public bool has_more = true;
+            #region Public Fields
+
             public comment[] comments;
+            public bool has_more = true;
             public int total_count;
+
+            #endregion Public Fields
+
+            #region Public Classes
 
             public class comment
             {
+                #region Public Fields
+
                 public int uid;
+
+                #endregion Public Fields
             }
+
+            #endregion Public Classes
         }
+
+        #endregion Private Classes
     }
 }
